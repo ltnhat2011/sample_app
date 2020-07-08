740bf93b9a9df69ada10d3a86a1a9a686b8a7f09
ltnhat2011 <ltnhat2011@gmail.com>
2020-07-08 08:14:18 +0700

    chapter-6 model-user completed

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: {maximum: 225},
+             format: {with: VALID_EMAIL_REGEX}
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+  has_secure_password
+end
